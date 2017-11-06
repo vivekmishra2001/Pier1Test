320be25301dcc8d6b96a121221e06418aff9a46c
vivemishra <vivemishra@USLMAVIVEMISHR2.us.deloitte.com>
2017-11-06 14:42:35 -0600

    checkin

--- GitTest/src/gitMain.java
+++ GitTest/src/gitMain.java
@@ -4,7 +4,7 @@ public class gitMain {
 	public static void main(String[] args) {
 		masterclass mas=new masterclass();
 		mas.gittest();
-		System.out.println("Main class mishra ");
+		System.out.println("Main class mishra");
 
 	}
 
